a55e0ff29ce29565b4eb828770e7afd981daf809
Buğra Görkem Özdağ <bugra.ozdag@vestel.com.tr>
2021-09-22 11:20:14 +0300

    first build

--- modbus-gui.py
+++ modbus-gui.py
@@ -722,10 +722,14 @@ class Ui_MainWindow(QObject):
             pass
 
 
-if __name__ == "__main__":
+def main():
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
